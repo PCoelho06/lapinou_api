c9ea356c77bebb452c61d596060763e51151d4fa
Pierre Da Silva Coelho <p.coelho@lapinou.tech>
2024-12-05 15:21:28 +0100

    Update antispam check in MessageController to validate key existence in request data

--- app/Controllers/MessageController.php
+++ app/Controllers/MessageController.php
@@ -44,7 +44,7 @@ class MessageController extends ResourceController
 
         $data = $this->request->getJSON(true);
 
-        if ($data['antispam'] != '') {
+        if (array_key_exists('antispam', $data)) {
             return $this->respondCreated($data);
         }
 
