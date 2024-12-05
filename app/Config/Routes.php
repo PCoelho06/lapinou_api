863575f91c46cd18a053e33e8f4104e87449e5ef
Pierre Da Silva Coelho <p.coelho@lapinou.tech>
2024-12-05 07:49:06 +0100

    Refactor routing configuration to group authentication and resource routes for better organization

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,12 +5,17 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->post('auth/register', 'AuthController::register');
-$routes->post('auth/login', 'AuthController::login');
 
-$routes->resource('v1/messages', ['controller' => 'MessageController', 'only' => ['index', 'show', 'create', 'delete']]);
-$routes->put('v1/messages/(:segment)/read', 'MessageController::markAsRead/$1');
-$routes->put('v1/messages/(:segment)/answered', 'MessageController::markAsAnswered/$1');
+$routes->group("auth", function (RouteCollection $routes) {
+    $routes->post('auth/register', 'AuthController::register');
+    $routes->post('auth/login', 'AuthController::login');
+});
 
-$routes->resource('v1/articles', ['controller' => 'ArticleController', 'only' => ['index', 'show', 'create', 'update', 'delete']]);
-$routes->resource('v1/categories', ['controller' => 'CategoryController', 'only' => ['index', 'show', 'create', 'update', 'delete']]);
+$routes->group('v1', function (RouteCollection $routes) {
+    $routes->resource('messages', ['controller' => 'MessageController', 'only' => ['index', 'show', 'create', 'delete']]);
+    $routes->put('messages/(:segment)/read', 'MessageController::markAsRead/$1');
+    $routes->put('messages/(:segment)/answered', 'MessageController::markAsAnswered/$1');
+
+    $routes->resource('articles', ['controller' => 'ArticleController', 'only' => ['index', 'show', 'create', 'update', 'delete']]);
+    $routes->resource('categories', ['controller' => 'CategoryController', 'only' => ['index', 'show', 'create', 'update', 'delete']]);
+});
